c3c5468cac2efef38dc2021178d283b0d68d5e1f
Jinxdimagine <martinvu403@gmail.com>
2024-03-08 13:19:47 +0100

    Created method that add animals to list and check if numbers of animals are smaller then 10

--- src/Barn.java
+++ src/Barn.java
@@ -8,5 +8,22 @@ public class Barn {
     public Barn(){
         AnimalList=new ArrayList<>(20);
     }
-
+   void addAnimal(Animal animal)throws Exception{
+        switch (animal.size){
+            case BIG:{
+                if (numberBiganimals<10){
+                    AnimalList.add(animal);
+                }else {
+                    throw new Exception("number of Big animals are at limit");
+                }
+            }
+            case SMALL:{
+                if (numberSmallanimals<10){
+                    AnimalList.add(animal);
+                }else {
+                    throw new Exception("number of Small animals are at limit");
+                }
+            }
+        }
+   }
 }
